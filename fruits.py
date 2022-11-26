7f2f6697e495c613b3e583c943a873ef6d9ca27c
uday <udaykadam26@gmail.com>
2022-11-26 12:20:53 +0530

    added program for summation

--- fruits.py
+++ fruits.py
@@ -2,3 +2,7 @@ print()[Dapp[C[C[D[D[D[D[D"[Capple")[C
 print(Orange"[D[D[D[D[D[D[D"[C[C[Corange")
 str1= "[D[Cbanana"[C
 str2=()[D""[Dgrapes")
+num1 = 3
+num2 = 2
+num3 = num1 + num2 
+print("The adding of two numbers is :" .format(num3))
